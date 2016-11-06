d163e05cd33955ce71021dc66392dd34f398eaff
tvanvier-GMU <tvanvier@gmu.edu>
2016-11-06 12:52:38 -0500

    Initial commit

---
+++ Assets/Cannon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannon : MonoBehaviour {
+
+	[System.Serializable]
+	public class CannonControls{
+		public KeyCode fireKey = KeyCode.Space;
+		public KeyCode rotateLeftKey = KeyCode.LeftArrow;
+		public KeyCode rotateRightKey = KeyCode.RightArrow;
+		public KeyCode rotateUpKey = KeyCode.UpArrow;
+		public KeyCode rotateDownKey = KeyCode.DownArrow;
+	}
+
+	public CannonControls controls = new CannonControls();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void LateUpdate(){
+			
+	}
+
+	void Fire(){
+	}
+}
---
+++ Assets/DestroyAfterSeconds.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAfterSeconds : MonoBehaviour {
+	public float seconds = 10;
+	// Use this for initialization
+	void Start () {
+		Destroy (this.gameObject, seconds);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
